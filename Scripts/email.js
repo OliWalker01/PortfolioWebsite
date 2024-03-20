a44ef9bf1498006b0842135cf475df6585d08b7b
OliWalker01 <103366754+OliWalker01@users.noreply.github.com>
2024-03-20 16:40:04 +0000

    Added validation to contact me page

--- Scripts/email.js
+++ Scripts/email.js
@@ -6,6 +6,8 @@ const phoneNumber = document.getElementById("pnumber");
 const emailAddress = document.getElementById("email");
 const querySubject = document.getElementById("subject");
 
+const emailValid = true;
+
 function sendEmail() {
     const bodyMessage = `Full Name: ${firstName.value} ${secondName.value}<br> Company Name: ${companyName.value}<br> Phone Number: ${phoneNumber.value}<br> Email: ${emailAddress.value}<br> Subject: ${querySubject.value}<br>`;
 
@@ -15,20 +17,34 @@ function sendEmail() {
         return; // Exit the function if validation fails
     }
 
+    if (!emailAddress.value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        alert("Error! Cannot Send Message, email is invalid!")
+        return;
+    }
+
+    if (!phoneNumber.value.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)) {
+        alert("Error!, Cannot Send Message, phone number is invalid!")
+        return;
+    }
+
     Email.send({
-        SecureToken: "a18f3b56-30c3-44d6-96e3-e1a5ae9767c0",
-        To: "oliver.walker01@hotmail.co.uk",
-        From: "oliver.walker01@gmail.com",
-        Subject: "Contact Made from Portfolio",
+        Host: "smtp.elasticemail.com",
+        Username: "noreply.oliverwalkerportfolio@gmail.com",
+        Password: "0DD2E5CC277991C5A26B72C1EF54E8ED14AA",
+        To: 'oliver.walker01@hotmail.co.uk',
+        From: "noreply.oliverwalkerportfolio@gmail.com", 
         Body: bodyMessage
     }).then(
         message => alert("Message Sent Successfully")
     ).catch(
         error => alert("Failed to send message: " + error.message)
     );
+
+    location.replace("contactmesuccess.html")
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    
     sendEmail();
 });
